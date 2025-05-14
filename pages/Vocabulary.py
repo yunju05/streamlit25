d0e268c7f01ade113b21efd02fef6689ab81f33f
yunju05 <yunju.05.3.25@gmail.com>
2025-05-14 15:13:22 +0900

    Update Vocabulary.py

--- pages/Vocabulary.py
+++ pages/Vocabulary.py
@@ -6,7 +6,7 @@ import random
 
 st.write("🌱 Vocabulary learning")
 
-tab1, tab2, tab3 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice"])
+tab1, tab2, tab3, tab4 = st.tabs(["❄️ 1. Lesson: Word list", "❄️ 2. Activity: Listen to the word", "❄️ 3. Spelling practice", "TBA"])
 
 ######### TAB 1
 
